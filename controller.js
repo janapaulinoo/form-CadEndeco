f9cccba7fd51fbc4ea80d3a5bbc4303a64ade412
jana paulino <janainapaulino32145@gmail.com>
2023-09-29 11:08:58 -0300

    arrumamos o id no index e criamos a funçao para limpar o formulario

--- controller.js
+++ controller.js
@@ -1,1 +1,11 @@
 'use strict'; // modo restrito 
+//consumindo API de CEP, do ViaCep 
+// https://viacep.com.br/ 
+
+//função para limpar formulario 
+const limparFormulario = () => {
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
